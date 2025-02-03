8e58e86c00f26ac6c32974fdc8cdfaf728cf5188
habeshe <yfreheiwot@gmail.com>
2025-02-03 23:47:24 +0300

    Update Vanya and Fence.py

--- Vanya and Fence.py
+++ Vanya and Fence.py
@@ -1,12 +1,7 @@
-personnum=int(input("please enter number of person"))
-heightoffen=int(input("please enter fence height"))
-x=0
-for i in range(personnum):
-    i=int(input("inter each value"))
-    if i>heightoffen:
-       i=2
-    else:
-        i=1
-    x=x+i
-print(x)
-    
+user=[int(i) for i in input().split()]
+height=[int(i) for i in input().split()]
+c=0
+for i in height:
+    if user[1]<i:
+        c+=1
+print(c*2+(user[0]-c)*1)
